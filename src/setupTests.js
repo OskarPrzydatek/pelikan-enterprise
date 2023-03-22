609d1e5cd2ab34de737dc32232e8b0d3e395ff65
OskarPrzydatek <o.przydatek@wp.pl>
2023-03-22 10:25:32 +0100

    remove jest and jest libs (vitest used finaly in project)

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
 import matchers from '@testing-library/jest-dom/matchers';
-import 'jest-styled-components';
 import { cleanup } from '@testing-library/react';
 import { expect, afterEach } from 'vitest';
 
